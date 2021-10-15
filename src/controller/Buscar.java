82450e067dcf4d5b978dd6144946209a2c4ee43e
Fernando <59516876+fero1394@users.noreply.github.com>
2021-10-15 10:31:55 -0500

    Estoy realizando desarrollo en la pestaña buscar

--- src/controller/Buscar.java
+++ src/controller/Buscar.java
@@ -1,5 +1,5 @@
 
-package controler;
+package controller;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,6 +12,17 @@ public class Buscar {
     
     private final String elemento;
     
+    private String nombre;
+    private String cedula;
+    private String habitacion;
+    private String entrada;
+    private String salida;
+    private int cantidadPersonas;
+    private int dias;
+    private String garaje;
+    private double precioTotal;
+    private boolean encontrado;
+    
     
     public Buscar(String elemento){ 
         
@@ -20,11 +31,15 @@ public class Buscar {
     }
     
     
-    public String buscarCliente() throws ClassNotFoundException{
+    
+    
+    
+    public boolean EstaCliente() throws ClassNotFoundException{
+        
         
         try{
             
-            ObjectInputStream recuperando_fichero=new ObjectInputStream(new FileInputStream("C://Users//User//Documents//Documentos_FERNANDO//CURSOS_DE_PROGRAMACION//Curso JAVA//Acceso_Fichero//empleado.dat"));
+            ObjectInputStream recuperando_fichero=new ObjectInputStream(new FileInputStream("C://Users//User//Documents//Documentos_FERNANDO//CURSOS_DE_PROGRAMACION//Proyectos JAVA//Hotel//src//Archivos//Archivo.dat"));
             
             ArrayList <Reserva> listaCliente = (ArrayList <Reserva>) recuperando_fichero.readObject();
             
@@ -34,29 +49,87 @@ public class Buscar {
             for (int i = 0; i < listaCliente.size(); i++) {
             
                 if (listaCliente.get(i).nombreCliente().equalsIgnoreCase(elemento) || listaCliente.get(i).cedulaCliente().equalsIgnoreCase(elemento)){
+                    
+                    this.nombre = listaCliente.get(i).nombreCliente();
+                    this.cedula = listaCliente.get(i).cedulaCliente();
+                    this.habitacion = listaCliente.get(i).getHabitacion();
+                    this.entrada = listaCliente.get(i).getFechaEntrada();
+                    this.salida = listaCliente.get(i).getFechaSalida();
+                    this.cantidadPersonas = listaCliente.get(i).getCantidaPersonas();
+                    this.dias = listaCliente.get(i).getDias();
+                    this.garaje = listaCliente.get(i).getGarage();
+                    this.precioTotal = listaCliente.get(i).precioTotal();
+        
+                    this.encontrado=true;
 
-                    System.out.println(
-                     "\nNOMBRE: " + listaCliente.get(i).nombreCliente()
-                    + "\nCEDULA: " + listaCliente.get(i).cedulaCliente()
-                    + "\nHABITACION: " + listaCliente.get(i).getHabitacion()
-                    + "\nFECHA ENTRADA: " + listaCliente.get(i).getFechaEntrada()
-                    + "\nFECHA SALIDA: " + listaCliente.get(i).getFechaSalida()
-                    + "\nCANTIDAD PERSONAS: " + listaCliente.get(i).getCantidaPersonas()
-                    + "\nDIAS DE ESTADIA: " + listaCliente.get(i).getDias()
-                    + "\nGARAGE: " + listaCliente.get(i).getGarage()
-                    +"\n--------------------------"
-                    + "\nPRECIO TOTAL: " + listaCliente.get(i).precioTotal());
+                }
+                else{
+                    this.encontrado=false;
                 }
             }
         
             }catch(IOException e){
             
             JOptionPane.showMessageDialog(null, "Problemas con el fichero que contiene la lista de clientes");
-            
+        
         }
         
-        return "DISFRUTE SU ESTADIA";
+        return encontrado;
        
     }
     
+    public String getNombre(){
+        
+        return nombre;
+    }  
+
+    
+    public String getCedula(){
+        
+        return cedula;
+    }  
+
+    public String getHabitacion(){
+        
+       return habitacion;
+    }         
+    
+        public String getEntrada(){
+        
+        return entrada;
+    }  
+    
+        public String getSalida(){
+        
+        return salida;
+    }  
+        
+        
+    public int getCantPersonas(){
+        
+        return cantidadPersonas; 
+    }  
+    
+    public int getDias(){
+        
+        return dias;
+    }  
+        
+    public String getGaraje(){
+        
+        return garaje;
+    }  
+    
+    public double getPrecioTotal(){
+        
+        return precioTotal;
+    }  
+    
+    public boolean getEsta(){
+        
+        return encontrado;
+    }
+        
+        
+        
 }
